960db207314e0a162f9237626235d7e652666ddd
yigit6545 <yigitalya8@gmail.com>
2025-10-21 07:48:35 +0300

    Add GitHub Pages deployment configuration

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/mbp107dersdeneme/'
+    : '/',
+  transpileDependencies: true
+})
